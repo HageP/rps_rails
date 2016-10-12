bf8101549643c0ce0ef97da785194ccd412dc09e
HageP <phage@chicagobooth.edu>
2016-10-11 20:34:30 -0500

    DONE

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,6 +2,17 @@ class GameController < ApplicationController
   def rps
     @user_move = params[:the_move]
 
+    if @user_move == "rock"
+    @user_move = 1
+    elsif @user_move == "paper"
+     @user_move = 2
+    elsif @user_move == "scissors"
+    @user_move = 3
+    end
+
+
+
+
     # ===============================================================
     # Your code goes below.
     # The move the user chose is in the variable @user_move.
@@ -12,9 +23,22 @@ class GameController < ApplicationController
     # In the end, make sure you assign the correct values to the
     #   following two variables:
 
-    @computer_move = "Replace this string with the correct value."
+    @ai_move = rand(2..4)
 
-    @result = "Replace this string with the correct value."
+    if @user_move == @ai_move
+      @result =  "You tie"
+    elsif @user_move == @ai_move + 1
+      @result =  "You WIN!"
+    elsif @user_move == @ai_move - 2
+      @result =  "You WIN!"
+    elsif @user_move == @ai_move - 1
+      @result =  "You LOSE :("
+    elsif @user_move == @ai_move + 2
+      @result =  "You LOSE :("
+    else
+      @result =  "sheiss"
+    end
+
 
     # ===============================================================
     # Your code goes above.
